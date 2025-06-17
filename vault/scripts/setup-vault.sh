0ab6655de4bd1d606e29fe915dfefb43d0af09ec
Joe Coleman <joseph.coleman@redcloudtechnology.com>
2025-06-17 16:58:41 +0100

    Vault fixed

--- vault/scripts/setup-vault.sh
+++ vault/scripts/setup-vault.sh
@@ -11,10 +11,10 @@ set -euo pipefail
 
 
 export VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID:-"buildkite-is-cool"}"
-export VAULT_ADDRESS="http://127.0.0.1:8220"
+export VAULT_ADDRESS="http://127.0.0.1:8200"
 
 # enable secret mountpath kv
-vault secrets enable kv
+vault secrets enable kv || ([ $? -eq 2 ] && exit 0)
 
 # put some data in the kv store
 
@@ -22,28 +22,28 @@ vault kv put secret/buildkite/vault-buildkite-demo/env some_value="alpacas" && s
 vault kv put kv/buildkite/vault-buildkite-demo/env artifactory_user="test" && sleep 1
 
 # enable approle authentication
-vault auth enable approle && sleep 1
-vault auth enable jwt && sleep 1
+vault auth enable approle && sleep 1 || ([ $? -eq 2 ] && exit 0)
+vault auth enable jwt && sleep 1 || ([ $? -eq 2 ] && exit 0)
 
 vault write auth/jwt/config \
     oidc_discovery_url="https://agent.buildkite.com" \
     oidc_client_id="" \
-    oidc_client_secret=""
+    oidc_client_secret="" || ([ $? -eq 2 ] && exit 0)
 
 # create the policy that will be scoped to the token requested by the agent role
-cat ./vault/config/agent-policy.hcl | vault policy write buildkite -
+cat ./vault/config/agent-policy.hcl | vault policy write buildkite - || ([ $? -eq 2 ] && exit 0)
 
 sleep 1
 
 # Create our agent roles, one for approle and another for jwt and attach the agent policies created earlier
-vault write auth/approle/role/buildkite token_policies="buildkite" token_ttl=1h token_max_ttl=4h && sleep 1
+vault write auth/approle/role/buildkite token_policies="buildkite" token_ttl=1h token_max_ttl=4h && sleep 1 || ([ $? -eq 2 ] && exit 0)
 vault write auth/jwt/role/buildkite \
     role_type="jwt" \
     allowed_redirect_uris="http://localhost:8250/oidc/callback" \
     bound_audiences="https://buildkite.com/jeremy-test" \
     user_claim="job_id" \
     policies=buildkite \
-    ttl=1h
+    ttl=1h || ([ $? -eq 2 ] && exit 0)
 
 
 # Now we can collect our ROLE_ID and SECRET_ID
